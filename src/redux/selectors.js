82341d4cc2b24d8a130a065d9072e7e6c5d7b2ef
Vlad Deputat <deputatwladek@gmail.com>
2021-04-25 18:31:28 +0300

    done

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,26 +1,26 @@
-// import { createSelector } from 'reselect';
+import { createSelector } from 'reselect';
 
 const contactsSelector = state => state.contacts;
 const itemsSelector = state => state.contacts.items;
 const filterSelector = state => state.contacts.filter;
 
-// const filtredContactsSelector = createSelector([
-//   [itemsSelector, filterSelector],
-//   (contacts, filter) => {
-//     const normFilter = filter.toLowerCase();
+const filtredContactsSelector = createSelector(
+  [itemsSelector, filterSelector],
+  (contacts, filter) => {
+    const normFilter = filter.toLowerCase();
 
-//     return contacts.filter(({ name }) =>
-//       name.toLowerCase().includes(normFilter),
-//     );
-//   },
-// ]);
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normFilter),
+    );
+  },
+);
 
-const filtredContactsSelector = state => {
-  const items = itemsSelector(state);
-  const normFilter = filterSelector(state).toLowerCase();
+// const filtredContactsSelector = state => {
+//   const items = itemsSelector(state);
+//   const normFilter = filterSelector(state).toLowerCase();
 
-  return items.filter(item => item.name.toLowerCase().includes(normFilter));
-};
+//   return items.filter(item => item.name.toLowerCase().includes(normFilter));
+// };
 
 export {
   filterSelector,
